8009f3e17ccd75b3a5262fbf047f1422edecc7d7
ashwin1005 <ashwani201kumar@gmail.com>
2024-05-30 05:16:16 +0530

    port-folio

---
+++ src/components/Contact.js
@@ -0,0 +1,35 @@
+import React from 'react';
+
+function Contact() {
+  return (
+    <section id='contact'>
+      <h2>Contact</h2>
+      <p>
+        Email:
+        <a href='mailto:ashwani201kumar@gmail.com'>ashwani201kumar@gmail.com</a>
+      </p>
+      <p>
+        LinkedIn:
+        <a
+          href='https://linkedin.com/in/ashwani-kumar201'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          linkedin.com/in/ashwani-kumar201
+        </a>
+      </p>
+      <p>
+        GitHub:
+        <a
+          href='https://github.com/ashwin1005'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          github.com/ashwin1005
+        </a>
+      </p>
+    </section>
+  );
+}
+
+export default Contact;
---
+++ src/components/Experience.js
@@ -0,0 +1,39 @@
+import React from 'react';
+
+function Experience() {
+  return (
+    <section id='experience'>
+      <h2>Experience</h2>
+      <div>
+        <h3>Software Engineer at Grey Chain Design, Delhi</h3>
+        <p>Nov 2022 – Jan 2024</p>
+        <ul>
+          <li>
+            Developed and maintained mobile applications using React Native.
+          </li>
+          <li>
+            Collaborated with cross-functional teams to design, implement, and
+            test new features.
+          </li>
+          <li>Provided QA support, built automation, and resolved bugs.</li>
+        </ul>
+      </div>
+      <div>
+        <h3>React Native Developer at Crownstack Technologies, Gurgaon</h3>
+        <p>Feb 2021 – Oct 2022</p>
+        <ul>
+          <li>Led the development of multiple mobile applications.</li>
+          <li>
+            Implemented Redux-Saga architecture for efficient data management.
+          </li>
+          <li>
+            Automated build processes using App Center and integrated
+            third-party APIs like Stripe.
+          </li>
+        </ul>
+      </div>
+    </section>
+  );
+}
+
+export default Experience;
