336a6f798314d893b586ba5f3f890a7015181233
ashwin1005 <ashwani201kumar@gmail.com>
2024-05-30 05:16:16 +0530

    port-folio

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import About from './components/About';
+import Experience from './components/Experience';
+import Projects from './components/Projects';
+import Skills from './components/Skills';
+import Contact from './components/Contact';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header>
+        <h1>Ashwani Kumar</h1>
+        <p>React Native Developer</p>
       </header>
+      <About />
+      <Experience />
+      <Projects />
+      <Skills />
+      <Contact />
     </div>
   );
 }
